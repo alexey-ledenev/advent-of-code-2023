a3fdf663a8b4f6e694c7102f977c2d814086dfd3
L <20566780+alexey-ledenev@users.noreply.github.com>
2023-12-10 21:55:11 +0300

    feat(day-4): part 1

---
+++ src/day-4/part-1.ts
@@ -0,0 +1,63 @@
+type Card = {
+  winning: Map<number, number>;
+  current: number[];
+};
+
+const toNumber = (n: string) => +n.trim();
+
+const setWinningMap = (map: Card["winning"], value: string) => {
+  if (value.length !== 0) {
+    const num = toNumber(value);
+    map.set(num, num);
+  }
+  return map;
+};
+
+const setCurrentList = (list: Card["current"], value: string) => {
+  if (value.length !== 0) {
+    list.push(toNumber(value));
+  }
+  return list;
+};
+
+const parseLineCard = (line: string): Card => {
+  const numbers = line.split(":")[1].trim();
+  const [winning, current] = numbers.split("|");
+  return {
+    winning: winning.trim().split(" ").reduce(
+      setWinningMap,
+      new Map<number, number>(),
+    ),
+    current: current.trim().split(" ").reduce(setCurrentList, []),
+  };
+};
+
+const calcCardPoints = (card: Card) => {
+  let points = 0;
+
+  const calcSucceedCount = (count: number, current: number) => {
+    if (card.winning.has(current)) {
+      return count + 1;
+    }
+    return count;
+  };
+
+  const count = card.current.reduce(calcSucceedCount, 0);
+
+  for (let i = 0; i < count; i++) {
+    if (i === 0) {
+      points += 1;
+    } else {
+      points *= 2;
+    }
+  }
+
+  return points;
+};
+
+const calcLine = (pointsSum: number, line: string) => {
+  const card = parseLineCard(line);
+  return pointsSum + calcCardPoints(card);
+};
+
+export const part1 = (lines: string[]) => lines.reduce(calcLine, 0);
